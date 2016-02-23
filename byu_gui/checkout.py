9c82b66ebb135332847cd6c7dbdc145e86c128c9
Samuel CG Giraud-Carrier <samuelgc@fearless.cs.byu.edu>
2016-02-23 08:43:49 -0700

    making it look pretty

--- byu_gui/checkout.py
+++ byu_gui/checkout.py
@@ -1,10 +1,21 @@
 import sys
+import os
 from PyQt4 import QtGui
 from byuam.project import Project
 from byuam.environment import Department
 		
 CHECKOUT_WINDOW_WIDTH = 340
 CHECKOUT_WINDOW_HEIGHT = 575
+stylesheet = """
+	        QWidget {
+	            background-color:#2E2E2E;
+	            color: white;
+	        }
+	        QLineEdit {
+			    background-color: black;
+	        }
+	    """
+
 dept_list = ['model', 'rig', 'anim', 'layout']
 
 class checkoutWindow(QtGui.QTabWidget):
@@ -17,26 +28,26 @@ class checkoutWindow(QtGui.QTabWidget):
 		#define gui elements
 		self.setWindowTitle('Checkout')
 		self.resize(CHECKOUT_WINDOW_WIDTH, CHECKOUT_WINDOW_HEIGHT)
+		self.setStyleSheet(stylesheet)
 
 		#create tabs
 		self.dept_tabs = QtGui.QTabWidget()
 		for dept in dept_list:
-			asset_list = QtGui.QListWidget()
-			for asset in self.project.list_assets():
-				item = QtGui.QListWidgetItem(asset)
-				asset_list.addItem(item)
-				shot_list = QtGui.QListWidget()
-			for shot in self.project.list_shots():
-				item = QtGui.QListWidgetItem(shot)
-				shot_list.addItem(item)
 			if dept in Department.ALL:
 				tab = QtGui.QWidget()
 				self.dept_tabs.addTab(tab, dept)
 				tab_layout = QtGui.QVBoxLayout()
+				element_list = QtGui.QListWidget()
+				
 				if dept in Department.FRONTEND:
-					tab_layout.addWidget(asset_list)
+					for asset in self.project.list_assets():
+						item = QtGui.QListWidgetItem(asset)
+						element_list.addItem(item)
 				elif dept in Department.BACKEND:
-					tab_layout.addWidget(shot_list)
+					for shot in self.project.list_shots():
+						item = QtGui.QListWidgetItem(shot)
+						element_list.addItem(item)
+				tab_layout.addWidget(element_list)
 				tab.setLayout(tab_layout)
 			else:
 				print("Not a valid Department")
@@ -50,12 +61,18 @@ class checkoutWindow(QtGui.QTabWidget):
 		#create button layout
 		button_layout = QtGui.QHBoxLayout()
 		button_layout.setSpacing(2)
-		
 		button_layout.addWidget(self.checkout_button)
 		button_layout.addWidget(self.cancel_button)
-		
+
+		self.img = QtGui.QLabel()
+		pixmap = QtGui.QPixmap(os.getcwd() + '/assets/images/taijitu.jpg')
+		scaled = pixmap.scaledToWidth(self.size().width())
+		self.img.setPixmap(scaled)
+
+		#create main layout
 		main_layout = QtGui.QVBoxLayout()
 		self.setLayout(main_layout)
+		main_layout.addWidget(self.img)
 		main_layout.setSpacing(5)
 		main_layout.setMargin(6)
 		main_layout.addWidget(self.dept_tabs)
