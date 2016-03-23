6d0a3af319bd62f8fb22c5046c28d4e0da5c2203
Trevor Cale Barrus <tcbarrus@godfather.cs.byu.edu>
2016-03-23 17:31:19 -0600

    Inserted checkout gui functionality inside maya

--- byugui/checkout_gui.py
+++ byugui/checkout_gui.py
@@ -1,4 +1,4 @@
-# Author: Samuel 
+# Author: Trevor Barrus
 
 import sys
 import os
@@ -6,51 +6,50 @@ from PyQt4 import QtGui, QtCore
 from byuam.project import Project
 from byuam.environment import Department, Environment
 
-WINDOW_WIDTH = 600
+WINDOW_WIDTH = 650
 WINDOW_HEIGHT = 600
 
-dept_list = Department.FRONTEND
+dept_list = Department.ALL
+        
+class CheckoutWindow(QtGui.QWidget):
 
-class CheckoutWindow(QtGui.QTabWidget):
-    
     finished = QtCore.pyqtSignal()
-    
+
+    ASSET_INDEX = 0
+    SHOT_INDEX = 1
+
     def __init__(self, parent):
         super(CheckoutWindow, self).__init__()
         self.parent = parent
         self.project = Project()
         self.environment = Environment()
         self.initUI()
-
+        
     def initUI(self):
         #define gui elements
+        self.resize(WINDOW_WIDTH,WINDOW_HEIGHT)
         self.setWindowTitle('Checkout')
-        self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
-
-        #create tabs
         self.dept_tabs = QtGui.QTabWidget()
+        #create tabs
         for dept in dept_list:
-            if dept in Department.ALL:
-                tab = QtGui.QWidget()
-                self.dept_tabs.addTab(tab, dept)
-                tab_layout = QtGui.QVBoxLayout()
-                element_list = QtGui.QListWidget()
+            tab = DepartmentTab(dept, self)
+            self.dept_tabs.insertTab(self.ASSET_INDEX, tab, dept)
+            tab_layout = QtGui.QVBoxLayout()
+            element_list = QtGui.QListWidget()
 				
-                if dept in Department.FRONTEND:
-                    for asset in self.project.list_assets():
-                        item = QtGui.QListWidgetItem(asset)
-                        element_list.addItem(item)
-                        element_list.currentItemChanged.connect(self.set_current_item)
-                elif dept in Department.BACKEND:
-                    for shot in self.project.list_shots():
-                        item = QtGui.QListWidgetItem(shot)
-                        element_list.addItem(item)
-                        element_list.currentItemChanged.connect(self.set_current_item)
-                tab_layout.addWidget(element_list)
-                tab.setLayout(tab_layout)
-            else:
-                print("Not a valid Department")
-
+            if dept in Department.FRONTEND:
+                for asset in self.project.list_assets():
+                    item = QtGui.QListWidgetItem(asset)
+                    element_list.addItem(item)
+                    element_list.currentItemChanged.connect(self.set_current_item)
+            elif dept in Department.BACKEND:
+                for shot in self.project.list_shots():
+                    item = QtGui.QListWidgetItem(shot)
+                    element_list.addItem(item)
+                    element_list.currentItemChanged.connect(self.set_current_item)
+            tab_layout.addWidget(element_list)
+            tab.setLayout(tab_layout)
+            
         #create buttons
         self.checkout_button = QtGui.QPushButton('Checkout')
         self.checkout_button.clicked.connect(self.checkout)
@@ -59,7 +58,7 @@ class CheckoutWindow(QtGui.QTabWidget):
 		
         #create button layout
         button_layout = QtGui.QHBoxLayout()
-        button_layout.setSpacing(2)
+        #button_layout.setSpacing(2)
         button_layout.addWidget(self.checkout_button)
         button_layout.addWidget(self.cancel_button)
 
@@ -76,17 +75,16 @@ class CheckoutWindow(QtGui.QTabWidget):
         main_layout.setMargin(6)
         main_layout.addWidget(self.dept_tabs)
         main_layout.addLayout(button_layout)
-        print 'ABOUT TO SHOW'
+            
         self.show()
-        
-        
+            
     def set_current_item(self, index):
         current_dept = dept_list[self.dept_tabs.currentIndex()]
         if current_dept in Department.FRONTEND:
             self.current_item = str(index.text())
         elif current_dept in Department.BACKEND:
             self.current_item = str(index.text())
-
+            
     def checkout(self):
         """
         Checks out the currently selected item
@@ -98,14 +96,26 @@ class CheckoutWindow(QtGui.QTabWidget):
         element_obj = asset_obj.get_element(current_dept)
         element_path = element_obj.checkout(current_user)
         if element_path != None:
-            app.quit()
+            self.parent.close()
             
+
     def closeEvent(self, event):
         self.finished.emit()
         event.accept()
-            
+        
+class DepartmentTab(QtGui.QWidget):
+    def __init__(self, department, parent):
+        super(DepartmentTab, self).__init__()
+        self.parent = parent
+        self.department = department
+        self.initUI()
+        
+    def initUI(self):
+        #define gui elements
+        self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
+    
         
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    ex = CheckoutWindow()
+    ex = CheckoutWindow(app)
     sys.exit(app.exec_())
