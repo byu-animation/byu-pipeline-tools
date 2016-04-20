d55c3d3d6ed068417049a735f299a4ab670d8876
Daniel Phelps <phelps.db@gmail.com>
2016-04-20 14:54:51 -0600

    More modifications, still dummy data

--- byugui/rollback_widget.py
+++ byugui/rollback_widget.py
@@ -3,10 +3,11 @@ import sys
 from PyQt4 import QtCore
 from PyQt4.QtGui import *
 
-WIDTH = 300
-HEIGHT = 600
+WIDTH = 400
+HEIGHT = 500
 
 class RollbackWidget(QWidget):
+
 	def __init__(self, parent):
 		super(RollbackWidget, self).__init__()
 		self.parent = parent
@@ -21,29 +22,58 @@ class RollbackWidget(QWidget):
 		self.versionLabel = QLabel()
 		self.authorLabel = QLabel()
 		self.commentLabel = QLabel()
+		self.commentLabel.setWordWrap(True)
 		detailView = QVBoxLayout()
-		detailView.addWidget(self.versionLabel)
-		detailView.addWidget(self.authorLabel)
+		detailView.addWidget(self.versionLabel, alignment=QtCore.Qt.AlignHCenter)
+		detailView.addStretch(1)
+		detailView.addWidget(self.authorLabel, alignment=QtCore.Qt.AlignHCenter)
+		detailView.addStretch(1)
 		detailView.addWidget(self.commentLabel)
-		detailView.addWidget(QPushButton('Rollback'))
+		detailView.addStretch(4)
+		rollbackBtn = QPushButton('Rollback to this Version')
+		rollbackBtn.clicked.connect(lambda: self._rollback_to_version(self.versionsList.currentItem()))
+		detailView.addWidget(rollbackBtn)
+		detailView.addStretch(1)
+		cancel = QPushButton('Cancel')
+		cancel.clicked.connect(self._cancel)
+		detailView.addWidget(cancel)
 
 		hbox = QHBoxLayout()
-		hbox.addStretch(1)
 		hbox.addWidget(self.versionsList)
 		hbox.addLayout(detailView)
+		hbox.setStretchFactor(self.versionsList, 1)
+		hbox.setStretchFactor(detailView, 2)
 
 		self.setLayout(hbox)
+
+		self.versions = []
 		self._fill_dummy_data()
 		self.show()
 
 	def _fill_dummy_data(self):
-		dummy_items = ['Item %s' % (i + 1) for i in range(10)]
-		self.versionsList.addItems(dummy_items)
+		for i in range(20):
+			v = VersionInfo('Revision %s' % (i + 1), "Bob Jenkins\n(bob@jenkins.com)", "Changed some stuff and did some things to make the whatever be better")
+			self.versions = [v] + self.versions
+			item = QListWidgetItem(v.version)
+			self.versionsList.insertItem(0, item)
 
 	def _update_detail_view(self, currentSelection, prevSelection):
-		self.versionLabel.setText(currentSelection.text())
-		self.authorLabel.setText('Bob Jenkins')
-		self.commentLabel.setText('Change the stuff and things to do whatever')
+		selectedVersion = self.versions[self.versionsList.currentRow()]
+		self.versionLabel.setText(selectedVersion.version)
+		self.authorLabel.setText(selectedVersion.author)
+		self.commentLabel.setText(selectedVersion.comment)
+
+	def _rollback_to_version(self, version):
+		print "Not yet implemented"
+
+	def _cancel(self):
+		self.close()
+
+class VersionInfo:
+	def __init__(self, version, author, comment):
+		self.version = version
+		self.author = author
+		self.comment = comment
 
 
 if __name__ == '__main__':
