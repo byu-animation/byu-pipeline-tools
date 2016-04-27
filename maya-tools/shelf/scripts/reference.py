3dc0e7dddeb10a8cb35e10ee2bf916d161c8aef8
jmoborn <jmoborn@yahoo.com>
2016-04-27 16:53:49 -0600

    reference tool bug fix

--- maya-tools/shelf/scripts/reference.py
+++ maya-tools/shelf/scripts/reference.py
@@ -1,8 +1,9 @@
 from byugui.reference_gui import ReferenceWindow
 from byuam.environment import Department
 import maya.cmds as cmds
-from PyQt4 import QtCore
+from PyQt4 import QtCore, QtGui
 import maya.OpenMayaUI as omu
+import os
 import sip
 
 maya_reference_dialog = None
@@ -15,8 +16,13 @@ def post_reference():
     file_path = maya_reference_dialog.filePath
     done = maya_reference_dialog.done
     reference = maya_reference_dialog.reference
+
     if file_path is not None and reference:
-        cmds.file(file_path, reference=True)
+        if os.path.exists(file_path):
+            cmds.file(file_path, reference=True)
+        else:
+            error_dialog = QtGui.QErrorMessage(maya_main_window())
+            error_dialog.showMessage("The chosen element is empty. Nothing has been published to it, so it can't be referenced.")
     if not done:
         go()
 
@@ -25,5 +31,5 @@ def go():
     parent = maya_main_window()
     filePath = cmds.file(q=True, sceneName=True)
     global maya_reference_dialog
-    maya_reference_dialog = ReferenceWindow(parent, filePath, [Department.MODEL, Department.RIG, Department.LAYOUT, Department.ANIM])
+    maya_reference_dialog = ReferenceWindow(parent, filePath, [Department.RIG, Department.MODEL])
     maya_reference_dialog.finished.connect(post_reference)
