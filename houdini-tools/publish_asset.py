c568922631485554daf48da823b811dd5656567b
tcbarrus <tcbarrus@gmail.com>
2016-06-20 09:33:45 -0600

    Modified houdini publish to automatically save node definition and match digital asset to definition

--- houdini-tools/publish_asset.py
+++ houdini-tools/publish_asset.py
@@ -13,35 +13,41 @@ def publish_hda():
         comment = publish_window.comment
         
         if is_asset:
-            if asset in project.list_assets():
-                body = project.get_asset(asset)
+            if asset_name in project.list_assets():
+                body = project.get_asset(asset_name)
                 
             if os.path.exists(src):
                 if body is not None:
                     if Element.DEFAULT_NAME in body.list_elements(Department.ASSEMBLY):
+                        #save node definition
+                        asset.type().definition().updateFromNode(asset)
+                        asset.matchCurrentDefinition()
                         element = body.get_element(Department.ASSEMBLY, Element.DEFAULT_NAME)
                         element.publish(user, src, comment)		
             else:
                 hou.ui.displayMessage("File does not exist")
                 
-        #TODO - Add logic for publishing shot
-        #else:
         
 def go():
     global publish_window
     global asset
+    global asset_name
     global src
     global is_asset
     is_asset = False
     
     nodes = hou.selectedNodes()
     if len(nodes) == 1:
-        is_asset = True
-        asset = nodes[0].type().name() #get name of asset
-        index = asset.find("_main")
-        asset = asset[:index]
-        src = nodes[0].type().definition().libraryFilePath()
-        publish_window = PublishWindow("", hou.ui.mainQtWindow(), [Department.ASSEMBLY])
+        if nodes[0].type().definition() is not None:
+            is_asset = True
+            asset = nodes[0]
+            asset_name = nodes[0].type().name() #get name of asset
+            index = asset_name.find("_main")
+            asset_name = asset_name[:index]
+            src = nodes[0].type().definition().libraryFilePath()
+            publish_window = PublishWindow("", hou.ui.mainQtWindow(), [Department.ASSEMBLY])
+        else:
+            hou.ui.displayMessage("Node is not a digital asset")
     
     else:
         publish_window = PublishWindow("", hou.ui.mainQtWindow(), [Department.LIGHTING])
