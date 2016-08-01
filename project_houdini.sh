283b6d65e49277efaabd11eac4eb2c1f85c8e906
tcbarrus <tcbarrus@gmail.com>
2016-08-01 09:52:44 -0600

    Updated houdini directory for new image

--- project_houdini.sh
+++ project_houdini.sh
@@ -6,7 +6,7 @@
 if [ -z "${HFS}" ]
 then
     # The default HFS directory if it isn't already defined
-    export HFS=/opt/hfs.current
+    export HFS=/opt/hfs15.5.480
 fi
 
 # source current houdini setup
