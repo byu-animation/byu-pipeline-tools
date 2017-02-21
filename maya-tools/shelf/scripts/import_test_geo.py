85c757d74c536d3d0e5314ac2f20c31e656b109d
Benjamin Nelson DeMann <bdemann@charming.cs.byu.edu>
2017-02-21 14:13:39 -0700

    Finished test geo tool for maya

--- maya-tools/shelf/scripts/import_test_geo.py
+++ maya-tools/shelf/scripts/import_test_geo.py
@@ -4,6 +4,5 @@ import os
 
 
 def go():
-    path = os.environ['BYU_TOOLS_DIR'] + '/byugui/assets/'
-    cmds.file("/users/animation/bdemann/Documents/grendel-dev/byu-pipeline-tools/byugui/assets/gnome.obj", reference=False, typ="OBJ")
-    cmds.file( path + "gnome.obj", reference=False, typ="OBJ" )
+    path = os.environ['BYU_TOOLS_DIR'] + '/maya-tools/shelf/geo/'
+    cmds.file( path + "gnome.obj", i = True, type="OBJ", iv=True, ra=True, mnc=False, ns="gnome", op="mo=1", pr=True)
