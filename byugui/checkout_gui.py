572ce1cffc836bf39b9d17cd40f2e280f413dd51
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2017-04-13 18:13:16 -0600

    Add option to to checkout window that toggles display of assets and shots without any publishes. Fix image in new asset window

--- byugui/checkout_gui.py
+++ byugui/checkout_gui.py
@@ -28,35 +28,22 @@ class CheckoutWindow(QtWidgets.QWidget):
         self.dept_tabs = QtWidgets.QTabWidget()
         self.dept_list = dept_list
         self.result = None
-        #create tabs
-        for dept in dept_list:
-            tab = DepartmentTab(self)
-            self.dept_tabs.addTab(tab, dept)
-            tab_layout = QtWidgets.QHBoxLayout()
-            element_list = QtWidgets.QListWidget()
-            commentBox = QtWidgets.QTextEdit()
-            commentBox.setReadOnly(True)
-            tab.commentBox = commentBox
 
-            if dept in Department.FRONTEND:
-                for asset in self.project.list_assets():
-                    item = QtWidgets.QListWidgetItem(asset)
-                    element_list.addItem(item)
-                    element_list.currentItemChanged.connect(self.set_current_item)
-            elif dept in Department.BACKEND:
-                for shot in self.project.list_shots():
-                    item = QtWidgets.QListWidgetItem(shot)
-                    element_list.addItem(item)
-                    element_list.currentItemChanged.connect(self.set_current_item)
-            tab_layout.addWidget(element_list)
-            tab_layout.addWidget(commentBox)
-            tab.setLayout(tab_layout)
+        #create checkbox to show only published assets
+        self.show_published = QtWidgets.QCheckBox("Display only assets or shots with previous publishes")
+        self.show_published.setCheckState(QtCore.Qt.Checked)
+        self.show_published.stateChanged.connect(self.changeBodyCheckoutVisibility)
+
+        #create Tabs
+        self.createTabs()
+        print "Tabs have been created"
 
         #create buttons
         self.checkout_button = QtWidgets.QPushButton('Checkout')
         self.checkout_button.clicked.connect(self.checkout)
         self.cancel_button = QtWidgets.QPushButton('Cancel')
         self.cancel_button.clicked.connect(self.close)
+        print "Buttons have been created"
 
         #create button layout
         button_layout = QtWidgets.QHBoxLayout()
@@ -75,9 +62,63 @@ class CheckoutWindow(QtWidgets.QWidget):
         main_layout.setSpacing(5)
         main_layout.setMargin(6)
         main_layout.addWidget(self.dept_tabs)
+        main_layout.addWidget(self.show_published)
         main_layout.addLayout(button_layout)
+        print "Layout have been created"
 
         self.show()
+
+    def createTabs(self):
+        #remember the current index so that we can restore it when we create the tabs
+        currIndex = self.dept_tabs.currentIndex()
+        #clear out the old tabs
+        self.dept_tabs.clear()
+        #create tabs
+        for dept in self.dept_list:
+            print "we are looking at " + dept
+            tab = DepartmentTab(self)
+            self.dept_tabs.addTab(tab, dept)
+            tab_layout = QtWidgets.QHBoxLayout()
+            element_list = QtWidgets.QListWidget()
+            commentBox = QtWidgets.QTextEdit()
+            commentBox.setReadOnly(True)
+            tab.commentBox = commentBox
+
+            if dept in Department.FRONTEND:
+                for asset in self.project.list_assets():
+                    if not self.show_published.isChecked() or self.hasPreviousPublish(asset, dept):
+                        print "We passed the first check"
+                        item = QtWidgets.QListWidgetItem(asset)
+                        element_list.addItem(item)
+                        element_list.currentItemChanged.connect(self.set_current_item)
+            elif dept in Department.BACKEND:
+                for shot in self.project.list_shots():
+                    if not self.show_published.isChecked() or self.hasPreviousPublish(shot, dept):
+                        print "We passed the second check"
+                        item = QtWidgets.QListWidgetItem(shot)
+                        element_list.addItem(item)
+                        element_list.currentItemChanged.connect(self.set_current_item)
+            tab_layout.addWidget(element_list)
+            tab_layout.addWidget(commentBox)
+            tab.setLayout(tab_layout)
+
+        #restore the previous index
+        self.dept_tabs.setCurrentIndex(currIndex)
+
+    def hasPreviousPublish(self, body, department):
+        print body
+        print department
+        asset_obj = self.project.get_body(body)
+        print asset_obj
+        element_obj = asset_obj.get_element(department)
+        last_publish = element_obj.get_last_publish()
+        if last_publish is None:
+            return False
+        return True
+
+    def changeBodyCheckoutVisibility(self):
+        #recreate tabs the with the new check option
+        self.createTabs()
 
     def set_current_item(self, index):
         current_dept = self.dept_list[self.dept_tabs.currentIndex()]
--- byugui/new_asset_gui.py
+++ byugui/new_asset_gui.py
@@ -7,7 +7,7 @@ from byuam.project import Project
 
 WINDOW_WIDTH = 300
 WINDOW_HEIGHT = 200
-        
+
 class CreateWindow(QtWidgets.QTabWidget):
 
     finished = QtCore.Signal()
@@ -19,19 +19,19 @@ class CreateWindow(QtWidgets.QTabWidget):
         super(CreateWindow, self).__init__()
         self.parent = parent
         self.initUI()
-        
+
     def initUI(self):
         #define gui elements
         self.setGeometry(300,300,WINDOW_WIDTH,WINDOW_HEIGHT)
         self.setWindowTitle('Create New Body')
-        
+
         #create tabs
         assetTab = NewAssetWindow('asset', self)
         shotTab = NewAssetWindow('shot', self)
-        
+
         self.insertTab(self.ASSET_INDEX, assetTab, 'Asset')
         self.insertTab(self.SHOT_INDEX, shotTab, 'Shot')
-        
+
         self.show()
 
     def accept(self):
@@ -45,7 +45,7 @@ class NewAssetWindow(QtWidgets.QWidget):
         self.parent = parent
         self.element = element
         self.initUI()
-        
+
     def initUI(self):
         #define gui elements
         self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -57,10 +57,10 @@ class NewAssetWindow(QtWidgets.QWidget):
         self.cancelBtn.clicked.connect(self.parent.close)
         #set image
         self.img = QtWidgets.QLabel()
-        pixmap = QtGui.QPixmap(os.environ['BYU_TOOLS_DIR'] + '/byugui/assets/images/taijitu.jpg')
+        pixmap = QtGui.QPixmap(os.environ['BYU_TOOLS_DIR'] + '/byugui/assets/images/film-banner.jpg')
         scaled = pixmap.scaledToWidth(self.size().width()/3)
         self.img.setPixmap(scaled)
-        
+
         #set gui layout
         grid = QtWidgets.QGridLayout(self)
         self.setLayout(grid)
@@ -69,7 +69,7 @@ class NewAssetWindow(QtWidgets.QWidget):
         grid.addWidget(self.textField, 1, 0, 1, 3)
         grid.addWidget(self.okBtn, 2, 1)
         grid.addWidget(self.cancelBtn, 2, 2)
-    
+
     #generate directories
     def createAsset(self):
         try:
@@ -84,13 +84,13 @@ class NewAssetWindow(QtWidgets.QWidget):
         except EnvironmentError, e:
             print e
             self.parent.accept()
-            
+
     def keyPressEvent(self, event):
         key = event.key()
         if key == QtCore.Qt.Key_Return:
             self.createAsset()
-    
-        
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     ex = CreateWindow()
