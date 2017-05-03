5bf3bdbe9130f6428278304e05a7bbf18413e623
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2017-05-03 16:05:10 -0600

    make hda creation automatic. add icon to hda

--- houdini-tools/assemble_asset.py
+++ houdini-tools/assemble_asset.py
@@ -3,13 +3,14 @@ import os
 # import pyqt_houdini
 from PySide2 import QtGui, QtWidgets, QtCore
 
-from byuam import Department, Project
+from byuam import Department, Project, Environment
 from byugui import CheckoutWindow
 
 def assemble_hda():
     asset_name = checkout_window.current_item
 
     project = Project()
+    environment = Environment()
     username = project.get_current_username()
     asset = project.get_asset(asset_name)
 
@@ -34,9 +35,15 @@ def assemble_hda():
         name = ''.join(geo_file.split('.')[:-1])
         geo.setName(name, unique_name=True)
 
-    subnet.setName(asset_name, unique_name=True)
+    hdaName = project.get_name() + "_" + asset_name
+    subnet.setName(hdaName, unique_name=True)
+
+    asset = subnet.createDigitalAsset(name=assembly.get_long_name())
+    assetTypeDef = asset.type().definition()
+    assetTypeDef.setIcon(environment.get_project_dir() + "/byu-pipeline-tools/assets/images/icons/hda-icon.png")
 
     # TODO: broken for now, need bug fix from sidefx
+    # TODO: I think that the three lines above this should work. I don't know what all of this does. The extra option seem a little unnessary. and I dont' know why we need to copy the type properties. Shouldn't it be that those properties come over when we create the asset in the first place? Why on earth are we trying to install it? It should already show up for the user and he hasn't publihsed it yet so it shouldn't be published for anyone else yet. - Ben DeMann
     # subnet.createDigitalAsset(name=assembly.get_long_name(), hda_file_name=checkout_file, description=asset_name)
 
     # subnet.type().definition().copyToHDAFile(checkout_file, new_name=assembly.get_long_name(), new_menu_name=asset_name)
