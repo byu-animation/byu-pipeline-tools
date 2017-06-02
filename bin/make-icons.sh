67f641d655f4c7d555a78316b25addec13e1fcea
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2017-06-02 13:39:35 -0600

    simplify the project creation process by adding a script to auto create desktop files, add email to project set up, update icons

--- bin/make-icons.sh
+++ bin/make-icons.sh
@@ -1,40 +1,92 @@
 #!/bin/bash
+usage() { echo "make-icon: [-n NICKNAME] projectName projectDir" 1>&2; exit; }
 
-shortProjectName=""
+nickname=""
 
-while getopts s: option
+while getopts n: option
 do
 	case "${option}"
 	in
-		s) shortProjectName=${OPTARG}
+		n) nickname=${OPTARG}
 		shift
 		shift
 		;;
+		*)
+		usage
 	esac
 done
 
 if [ "$1" == "" ]||[ "$2" == "" ]; then
-	echo "Error: Invalid input."
-    echo "Usage: make-icon.sh project_name project_dir"
-	exit 1
+	usage
 fi
 
 PROJECT_NAME="$1"
 PROJECT_PATH="$2"
 
-if [ "${shortProjectName}" != "" ]; then
-	sh ./icon.sh -n ${shortProjectName}aya ${PROJECT_NAME} ${PROJECT_PATH} Maya byu-maya.png project_maya.sh
-	sh ./icon.sh -n ${shortProjectName}ini ${PROJECT_NAME} ${PROJECT_PATH} Houdini byu-houdini.png project_houdini.sh
-	sh ./icon.sh -n ${shortProjectName}ari ${PROJECT_NAME} ${PROJECT_PATH} Mari byu-mari.png project_mari.sh
-	sh ./icon.sh -n ${shortProjectName}uke ${PROJECT_NAME} ${PROJECT_PATH} Nuke byu-nuke.png project_nuke.sh
-	sh ./icon.sh -n ${shortProjectName}ack ${PROJECT_NAME} ${PROJECT_PATH} Slack byu-slack.png project_slack.sh
+function icon {
+	icon_usage() { echo "icon: [-n NICKNAME] projectName projectDir scriptName scriptIcon script" 1>&2; exit; }
+	local nickname=""
+
+	local OPTIND n option
+	while getopts ":n:" option
+	do
+		case "${option}"
+		in
+			n) nickname=${OPTARG}
+			shift
+			shift
+			;;
+		esac
+	done
+
+	if [ "$1" == "" ]||[ "$2" == "" ]||[ "$3" == "" ]||[ "$4" == "" ]||[ "$5" == "" ]; then
+		icon_usage
+	fi
+
+	PROJECT_NAME="$1"
+	PROJECT_PATH="$2"
+	SOFTWARE_NAME="$3"
+	ICON="$4"
+	SCRIPT="$5"
+
+	if [ "$nickname" == "" ]; then
+		PROGRAM_NAME=${PROJECT_NAME}" "${SOFTWARE_NAME}
+	else
+		PROGRAM_NAME=$nickname
+	fi
+
+	NOSPACES=${SOFTWARE_NAME// /-}
+	FILENAME=${PROJECT_PATH}"/${NOSPACES,,}-byu"
+
+	echo "#!/usr/bin/env xdg-open" > ${FILENAME}.desktop
+	echo "[Desktop Entry]" >> ${FILENAME}.desktop
+	echo "Version=x.y" >> ${FILENAME}.desktop
+	echo "Name=${PROGRAM_NAME}" >> ${FILENAME}.desktop
+	echo "Name[en_US]=${PROGRAM_NAME}" >> ${FILENAME}.desktop
+	echo "Comment=BYU Pipeline Tools ${PROJECT_NAME} ${SOFTWARE_NAME}" >> ${FILENAME}.desktop
+	echo "Exec=${PROJECT_PATH}/byu-pipeline-tools/app-launch-scripts/${SCRIPT}" >> ${FILENAME}.desktop
+	echo "Icon=${PROJECT_PATH}/byu-pipeline-tools/assets/images/icons/${ICON}" >> ${FILENAME}.desktop
+	echo "Terminal=false" >> ${FILENAME}.desktop
+	echo "Type=Application" >> ${FILENAME}.desktop
+	echo "Categories=Utility;Application;" >> ${FILENAME}.desktop
+
+	chmod 770 ${FILENAME}.desktop
+}
+
+
+if [ "${nickname}" != "" ]; then
+	icon -n "${nickname}aya" ${PROJECT_NAME} ${PROJECT_PATH} Maya byu-maya.png project_maya.sh
+	icon -n "${nickname}ini" ${PROJECT_NAME} ${PROJECT_PATH} Houdini byu-houdini.png project_houdini.sh
+	icon -n "${nickname}ari" ${PROJECT_NAME} ${PROJECT_PATH} Mari byu-mari.png project_mari.sh
+	icon -n "${nickname}uke" ${PROJECT_NAME} ${PROJECT_PATH} Nuke byu-nuke.png project_nuke.sh
+	icon -n "${nickname}ack" ${PROJECT_NAME} ${PROJECT_PATH} Slack byu-slack.png project_slack.sh
 else
-	sh ./icon.sh ${PROJECT_NAME} ${PROJECT_PATH} Maya byu-maya.png project_maya.sh
-	sh ./icon.sh ${PROJECT_NAME} ${PROJECT_PATH} Houdini byu-houdini.png project_houdini.sh
-	sh ./icon.sh ${PROJECT_NAME} ${PROJECT_PATH} Mari byu-mari.png project_mari.sh
-	sh ./icon.sh ${PROJECT_NAME} ${PROJECT_PATH} Nuke byu-nuke.png project_nuke.sh
-	sh ./icon.sh ${PROJECT_NAME} ${PROJECT_PATH} Slack byu-slack.png project_slack.sh
+	icon ${PROJECT_NAME} ${PROJECT_PATH} Maya byu-maya.png project_maya.sh
+	icon ${PROJECT_NAME} ${PROJECT_PATH} Houdini byu-houdini.png project_houdini.sh
+	icon ${PROJECT_NAME} ${PROJECT_PATH} Mari byu-mari.png project_mari.sh
+	icon ${PROJECT_NAME} ${PROJECT_PATH} Nuke byu-nuke.png project_nuke.sh
+	icon ${PROJECT_NAME} ${PROJECT_PATH} Slack byu-slack.png project_slack.sh
 fi
 
-sh ./icon.sh -n "Pipeline Instructions" ${PROJECT_NAME} ${PROJECT_PATH} "Pipeline Instructions" byu-how-to.png project_how_to.sh
-sh ./icon.sh ${PROJECT_NAME} ${PROJECT_PATH} Browser byu-browser.png project_browser.sh
+icon -n "Pipeline Instructions" ${PROJECT_NAME} ${PROJECT_PATH} "Pipeline Instructions" byu-how-to.png project_how_to.sh
+icon ${PROJECT_NAME} ${PROJECT_PATH} Browser byu-browser.png project_browser.sh
--- byuam/environment.py
+++ byuam/environment.py
@@ -6,239 +6,242 @@ from . import pipeline_io
 
 class Environment:
 
-    PROJECT_ENV = "BYU_PROJECT_DIR"
-    PIPELINE_FILENAME = ".project"
+	PROJECT_ENV = "BYU_PROJECT_DIR"
+	PIPELINE_FILENAME = ".project"
 
-    PROJECT_NAME = "name"
-    PRODUCTION_DIR = "production_dir"
-    ASSETS_DIR = "assets_dir"
-    SHOTS_DIR = "shots_dir"
-    USERS_DIR = "users_dir"
-    ASSEMBLY_DIR = "assembly_dir"
-    EMAIL_ADDRESS = "email_address"
-    EMAIL_PASSWORD = "email_password"
+	PROJECT_NAME = "name"
+	PRODUCTION_DIR = "production_dir"
+	ASSETS_DIR = "assets_dir"
+	SHOTS_DIR = "shots_dir"
+	USERS_DIR = "users_dir"
+	ASSEMBLY_DIR = "assembly_dir"
+	EMAIL_ADDRESS = "email_address"
+	EMAIL_PASSWORD = "email_password"
 
-    @staticmethod
-    def create_new_dict(name, assets_dir, shots_dir, users_dir, assembly_dir):
-        datadict = {}
-        datadict[Environment.PROJECT_NAME] = name
-        datadict[Environment.ASSETS_DIR] = assets_dir
-        datadict[Environment.SHOTS_DIR] = shots_dir
-        datadict[Environment.USERS_DIR] = users_dir
-        datadict[Environment.ASSEMBLY_DIR] = assembly_dir
-        return datadict
+	@staticmethod
+	def create_new_dict(name, assets_dir, shots_dir, users_dir, assembly_dir, email_address=None, email_password=None):
+		datadict = {}
+		datadict[Environment.PROJECT_NAME] = name
+		datadict[Environment.ASSETS_DIR] = assets_dir
+		datadict[Environment.SHOTS_DIR] = shots_dir
+		datadict[Environment.USERS_DIR] = users_dir
+		datadict[Environment.ASSEMBLY_DIR] = assembly_dir
+		if(email_address is not None and email_password is not None):
+			datadict[Environment.EMAIL_ADDRESS] = email_address
+			datadict[Environment.EMAIL_PASSWORD] = email_password
+		return datadict
 
-    def __init__(self):
-        """
-        Creates an Environment instance from data in the .project file in the directory defined by the
-        environment variable $BYU_PROJECT_DIR. If this variable is not defined or the .project file does
-        not exist inside it, an EnvironmentError is raised. Creates the workspace for the current user
-        if it doesn't already exist.
-        """
-        self._project_dir = os.getenv(Environment.PROJECT_ENV)
-        if self._project_dir is None:
-            raise EnvironmentError(Environment.PROJECT_ENV + " is not defined")
+	def __init__(self):
+		"""
+		Creates an Environment instance from data in the .project file in the directory defined by the
+		environment variable $BYU_PROJECT_DIR. If this variable is not defined or the .project file does
+		not exist inside it, an EnvironmentError is raised. Creates the workspace for the current user
+		if it doesn't already exist.
+		"""
+		self._project_dir = os.getenv(Environment.PROJECT_ENV)
+		if self._project_dir is None:
+			raise EnvironmentError(Environment.PROJECT_ENV + " is not defined")
 
-        project_file = os.path.join(self._project_dir, Environment.PIPELINE_FILENAME)
-        if not os.path.exists(project_file):
-            raise EnvironmentError(project_file + " does not exist")
-        self._datadict = pipeline_io.readfile(project_file)
-        self._current_username = getpass.getuser()
-        self._current_user_workspace = os.path.join(self.get_users_dir(), self._current_username)
-        self._create_user(self._current_username)
+		project_file = os.path.join(self._project_dir, Environment.PIPELINE_FILENAME)
+		if not os.path.exists(project_file):
+			raise EnvironmentError(project_file + " does not exist")
+		self._datadict = pipeline_io.readfile(project_file)
+		self._current_username = getpass.getuser()
+		self._current_user_workspace = os.path.join(self.get_users_dir(), self._current_username)
+		self._create_user(self._current_username)
 
-    def get_project_name(self):
-        """
-        return the name of the current project
-        """
-        return self._datadict[Environment.PROJECT_NAME]
+	def get_project_name(self):
+		"""
+		return the name of the current project
+		"""
+		return self._datadict[Environment.PROJECT_NAME]
 
-    def get_project_dir(self):
-        """
-        return the absolute filepath to the directory of the current project
-        """
-        return os.path.abspath(self._project_dir)
+	def get_project_dir(self):
+		"""
+		return the absolute filepath to the directory of the current project
+		"""
+		return os.path.abspath(self._project_dir)
 
-    def get_assets_dir(self):
-        """
-        return the absolute filepath to the assets directory of the current project
-        """
-        return os.path.abspath(self._datadict[Environment.ASSETS_DIR])
+	def get_assets_dir(self):
+		"""
+		return the absolute filepath to the assets directory of the current project
+		"""
+		return os.path.abspath(self._datadict[Environment.ASSETS_DIR])
 
-    def get_shots_dir(self):
-        """
-        return the absolute filepath to the shots directory of the current project
-        """
-        return os.path.abspath(self._datadict[Environment.SHOTS_DIR])
+	def get_shots_dir(self):
+		"""
+		return the absolute filepath to the shots directory of the current project
+		"""
+		return os.path.abspath(self._datadict[Environment.SHOTS_DIR])
 
-    def get_assembly_dir(self):
-        """
-        return the absolute filepath to the assembly directory of the current project
-        (in a houdini pipeline, this is the otls directory)
-        """
-        return os.path.abspath(self._datadict[Environment.ASSEMBLY_DIR])
+	def get_assembly_dir(self):
+		"""
+		return the absolute filepath to the assembly directory of the current project
+		(in a houdini pipeline, this is the otls directory)
+		"""
+		return os.path.abspath(self._datadict[Environment.ASSEMBLY_DIR])
 
-    def _create_user(self, username):
-        workspace = os.path.join(self.get_users_dir(), username)
-        if not os.path.exists(workspace):
-            pipeline_io.mkdir(workspace)
-        user_pipeline_file = os.path.join(workspace, User.PIPELINE_FILENAME)
-        if not os.path.exists(user_pipeline_file):
-            datadict = User.create_new_dict(username)
-            pipeline_io.writefile(user_pipeline_file, datadict)
-        # user = User(workspace)
-        # return user
+	def _create_user(self, username):
+		workspace = os.path.join(self.get_users_dir(), username)
+		if not os.path.exists(workspace):
+			pipeline_io.mkdir(workspace)
+		user_pipeline_file = os.path.join(workspace, User.PIPELINE_FILENAME)
+		if not os.path.exists(user_pipeline_file):
+			datadict = User.create_new_dict(username)
+			pipeline_io.writefile(user_pipeline_file, datadict)
+		# user = User(workspace)
+		# return user
 
-    def get_user(self, username=None):
-        if username is None:
-            username = self._current_username
-        user_filepath = os.path.join(self.get_users_dir(), username)
-        if not os.path.exists(user_filepath):
-            raise EnvironmentError("no such user "+str(username))
-        return User(user_filepath)
+	def get_user(self, username=None):
+		if username is None:
+			username = self._current_username
+		user_filepath = os.path.join(self.get_users_dir(), username)
+		if not os.path.exists(user_filepath):
+			raise EnvironmentError("no such user "+str(username))
+		return User(user_filepath)
 
-    def get_users_dir(self):
-        """
-        return the absolute filepath to the users directory of the current project
-        """
-        return os.path.abspath(self._datadict[Environment.USERS_DIR])
+	def get_users_dir(self):
+		"""
+		return the absolute filepath to the users directory of the current project
+		"""
+		return os.path.abspath(self._datadict[Environment.USERS_DIR])
 
-    def get_current_username(self):
+	def get_current_username(self):
 
-        return self._current_username
+		return self._current_username
 
-    def get_user_workspace(self, username=None):
-        """
-        return the given users workspace. If no user is given, return the current user's workspace.
-        """
-        if username is not None:
-            workspace = os.path.join(self.get_users_dir, username)
-            if not os.path.exists(workspace):
-                pipeline_io.mkdir(workspace)
-            return workspace
-        else:
-            return self._current_user_workspace
+	def get_user_workspace(self, username=None):
+		"""
+		return the given users workspace. If no user is given, return the current user's workspace.
+		"""
+		if username is not None:
+			workspace = os.path.join(self.get_users_dir, username)
+			if not os.path.exists(workspace):
+				pipeline_io.mkdir(workspace)
+			return workspace
+		else:
+			return self._current_user_workspace
 
-    def sendmail(self, dst_addresses, subject, message):
-        """
-        send an email from the project email account to the given dst_addresses with the given subject and message.
-        dst_addresses -- list of strings destination email addresses
-        subject -- string subject line of the email
-        message -- string body of the email
-        """
-        if(Environment.EMAIL_ADDRESS in self._datadict and Environment.EMAIL_PASSWORD in self._datadict):
-            pipeline_io.sendmail(dst_addresses, subject, message,
-                                 self._datadict[Environment.EMAIL_ADDRESS],
-                                 self._datadict[Environment.EMAIL_PASSWORD],
-                                 self.get_project_name()+" Support")
+	def sendmail(self, dst_addresses, subject, message):
+		"""
+		send an email from the project email account to the given dst_addresses with the given subject and message.
+		dst_addresses -- list of strings destination email addresses
+		subject -- string subject line of the email
+		message -- string body of the email
+		"""
+		if(Environment.EMAIL_ADDRESS in self._datadict and Environment.EMAIL_PASSWORD in self._datadict):
+			pipeline_io.sendmail(dst_addresses, subject, message,
+								 self._datadict[Environment.EMAIL_ADDRESS],
+								 self._datadict[Environment.EMAIL_PASSWORD],
+								 self.get_project_name()+" Support")
 
 
 class User:
-    """
-    The User class holds information about a user, this will be used a lot more for the web site
-    """
+	"""
+	The User class holds information about a user, this will be used a lot more for the web site
+	"""
 
-    NAME = "name"
-    EMAIL = "email"
-    CSID = "csid"
+	NAME = "name"
+	EMAIL = "email"
+	CSID = "csid"
 
-    PIPELINE_FILENAME = ".user"
+	PIPELINE_FILENAME = ".user"
 
-    @staticmethod
-    def create_new_dict(username):
-        datadict = {}
-        datadict[User.CSID] = username
-        name = pwd.getpwnam(username).pw_gecos
-        datadict[User.NAME] = name
-        datadict[User.EMAIL] = ""
-        return datadict
+	@staticmethod
+	def create_new_dict(username):
+		datadict = {}
+		datadict[User.CSID] = username
+		name = pwd.getpwnam(username).pw_gecos
+		datadict[User.NAME] = name
+		datadict[User.EMAIL] = ""
+		return datadict
 
-    def __init__(self, filepath):
-        self._filepath = filepath
-        self._pipeline_file = os.path.join(self._filepath, self.PIPELINE_FILENAME)
-        if not os.path.exists(self._pipeline_file):
-            raise EnvironmentError("invalid user file: " + self._pipeline_file + " does not exist")
-        self._datadict = pipeline_io.readfile(self._pipeline_file)
+	def __init__(self, filepath):
+		self._filepath = filepath
+		self._pipeline_file = os.path.join(self._filepath, self.PIPELINE_FILENAME)
+		if not os.path.exists(self._pipeline_file):
+			raise EnvironmentError("invalid user file: " + self._pipeline_file + " does not exist")
+		self._datadict = pipeline_io.readfile(self._pipeline_file)
 
-    def get_username(self):
-        return self._datadict[self.CSID]
+	def get_username(self):
+		return self._datadict[self.CSID]
 
-    def get_fullname(self):
-        return self._datadict[self.NAME]
+	def get_fullname(self):
+		return self._datadict[self.NAME]
 
-    def get_email(self):
-        return self._datadict[self.EMAIL]
+	def get_email(self):
+		return self._datadict[self.EMAIL]
 
-    def has_email(self):
-        return self._datadict[self.EMAIL] != ""
+	def has_email(self):
+		return self._datadict[self.EMAIL] != ""
 
-    def update_email(self, new_email):
-        self._datadict[self.EMAIL] =  new_email
-        pipeline_io.writefile(self._pipeline_file, self._datadict)
+	def update_email(self, new_email):
+		self._datadict[self.EMAIL] =  new_email
+		pipeline_io.writefile(self._pipeline_file, self._datadict)
 
-    def update_fullname(self, new_name):
-        self._datadict[self.NAME] = new_name
-        pipeline_io.writefile(self._pipeline_file, self._datadict)
+	def update_fullname(self, new_name):
+		self._datadict[self.NAME] = new_name
+		pipeline_io.writefile(self._pipeline_file, self._datadict)
 
 
 class Department:
-    """
-    Class describing departments that work on a project.
-    """
+	"""
+	Class describing departments that work on a project.
+	"""
 
-    DESIGN = "design"
-    MODEL = "model"
-    RIG = "rig"
-    ASSEMBLY = "assembly"
-    LAYOUT = "layout"
-    ANIM = "anim"
-    TEXTURE = "texture"
-    MATERIAL = "material"
-    CFX = "cfx"
-    FX = "fx"
-    LIGHTING = "lighting"
-    RENDER = "render"
-    COMP = "comp"
-    ASSET_DEPTS = [DESIGN, MODEL, RIG, TEXTURE, MATERIAL, ASSEMBLY]
-    SHOT_DEPTS = [LAYOUT, ANIM, CFX, FX, LIGHTING, RENDER, COMP]
-    ALL = [DESIGN, MODEL, RIG, TEXTURE, MATERIAL, ASSEMBLY, LAYOUT, ANIM, CFX, FX, LIGHTING, RENDER, COMP]
+	DESIGN = "design"
+	MODEL = "model"
+	RIG = "rig"
+	ASSEMBLY = "assembly"
+	LAYOUT = "layout"
+	ANIM = "anim"
+	TEXTURE = "texture"
+	MATERIAL = "material"
+	CFX = "cfx"
+	FX = "fx"
+	LIGHTING = "lighting"
+	RENDER = "render"
+	COMP = "comp"
+	ASSET_DEPTS = [DESIGN, MODEL, RIG, TEXTURE, MATERIAL, ASSEMBLY]
+	SHOT_DEPTS = [LAYOUT, ANIM, CFX, FX, LIGHTING, RENDER, COMP]
+	ALL = [DESIGN, MODEL, RIG, TEXTURE, MATERIAL, ASSEMBLY, LAYOUT, ANIM, CFX, FX, LIGHTING, RENDER, COMP]
 
 
 class Status:
-    """
-    Class describing status levels for elements.
-    """
+	"""
+	Class describing status levels for elements.
+	"""
 
-    WAIT = "wait"
-    READY = "ready"
-    STARTED = "started"
-    DONE = "done"
-    ALL = [WAIT, READY, STARTED, DONE]
+	WAIT = "wait"
+	READY = "ready"
+	STARTED = "started"
+	DONE = "done"
+	ALL = [WAIT, READY, STARTED, DONE]
 
-    def get_level(name):
-        """
-        given a status name return the equivalent level
-        e.g. WAIT    -> 0
-             READY   -> 1
-             STARTED -> 2
-             DONE    -> 3
-        """
-        return Status.ALL.index(status)
+	def get_level(name):
+		"""
+		given a status name return the equivalent level
+		e.g. WAIT	-> 0
+			 READY   -> 1
+			 STARTED -> 2
+			 DONE	-> 3
+		"""
+		return Status.ALL.index(status)
 
-    def get_name(level):
-        """
-        given a status level return the equivalent name
-        """
-        return Status.ALL[level]
+	def get_name(level):
+		"""
+		given a status level return the equivalent name
+		"""
+		return Status.ALL[level]
 
 
 class AssetType:
-    """
-    Class describing types of assets.
-    """
+	"""
+	Class describing types of assets.
+	"""
 
-    CHARACTER = "character"
-    SET = "set"
-    PROP = "prop"
-    ACCESSORY = "accessory"
-    ALL = [CHARACTER, SET, PROP, ACCESSORY]
+	CHARACTER = "character"
+	SET = "set"
+	PROP = "prop"
+	ACCESSORY = "accessory"
+	ALL = [CHARACTER, SET, PROP, ACCESSORY]
