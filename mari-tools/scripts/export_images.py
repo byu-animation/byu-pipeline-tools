379f30687149cc3bef77e7811c789c5e13fd521b
Benjamin DeMann <benjamin.demann@gmail.com>
2017-06-14 14:58:07 -0600

    added tex maker

--- mari-tools/scripts/export_images.py
+++ mari-tools/scripts/export_images.py
@@ -1,6 +1,7 @@
 from byuam import Department, Project, Environment
 from byugui.assemble_gui import AssembleWindow
 import os
+import subprocess
 import mari
 
 def go():
@@ -14,6 +15,10 @@ def exportTex():
 	exportImage(mari.geo.current(), asset_name, uvIndexList)
 	print "Export Texture"
 
+def makeTex(tif, tex):
+	txmake = "txmake " + tif + " " + tex
+	subprocess.Popen(txmake)
+
 def exportImage(geo, asset_name):
 	# Set up the project and environment
 	project = Project()
@@ -25,7 +30,7 @@ def exportImage(geo, asset_name):
 	# get the texture element and check it out
 	texture = asset.get_element(Department.TEXTURE)
 
-	baseDir = "/users/animation/bdemann/Desktop/mariTest/tif"
+	baseDir = "/users/animation/bdemann/Desktop/mariTest/"
 	# TODO : Figure out how to export the channel and not just the images from the channel
 	channels = geo.channelList()
 	for channel in channels:
@@ -39,7 +44,6 @@ def exportImage(geo, asset_name):
 			for uvIndex in uvIndices:
 				uvIndexList.add(unIndex)
 
-
 		channel_name = channel.name()
 		file_name = asset_name + "-" + channel_name + "$UDIM.tif"
 		file_path = os.path.join(baseDir, file_name)
@@ -47,4 +51,10 @@ def exportImage(geo, asset_name):
 
 		# find each exported file and convert it to a .tex file
 		for i in uvIndexList:
-			makeTex()
+			udim = i + 1001
+			file_name = asset_name + "-" channel_name + str(udim)
+			old_file = file_name + ".tif"
+			new_file = file_name + ".tex"
+			old_file_path = os.path.join(baseDir, old_file)
+			new_file_path = os.path.join(baseDir, old_file)
+			makeTex(old_file_path, new_file_path)
