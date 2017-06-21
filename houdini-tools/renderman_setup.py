49f73b74dec47441e0fb4446e3658d68a77da1bb
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2017-06-21 16:34:18 -0600

    fix

--- houdini-tools/renderman_setup.py
+++ houdini-tools/renderman_setup.py
@@ -4,12 +4,8 @@ import hou
 def go():
 	obj = hou.node('obj')
 	out = hou.node('out')
-	shop = hou.node('shop')
 
 	ris = out.createNode('ris')
-
-	risnet = shop.createNode('risnet')
-	risnet.createNode('pxrsurface')
 
 	dome = obj.createNode('pxrdomelight')
 	dome.setParms({'lightColorMap':'$JOB/byu-pipeline-tools/assets/lights/footprint-court.tex'})
