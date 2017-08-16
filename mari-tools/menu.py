18efd569130e569b458ebc6dbe50200f3b0bd7c9
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2017-08-16 10:47:46 -0600

    renamed the assemble button to be more intuitive

--- mari-tools/menu.py
+++ mari-tools/menu.py
@@ -10,7 +10,7 @@ checkout_button = mari.actions.create('Checkout', 'checkout.go()')
 export_all_button = mari.actions.create('Export All Tex', 'export_images.go(export_images.ALL)')
 export_geo_button = mari.actions.create('Export Selected Geo Tex', 'export_images.go(export_images.SELECTED_GEO)')
 export_channel_button = mari.actions.create('Export Selected Channel Tex', 'export_images.go(export_images.SELECTED_CHANNEL)')
-assemble_button = mari.actions.create('Assemble Texuture', 'assemble_texture.go()')
+assemble_button = mari.actions.create('Create Project from Geo', 'assemble_texture.go()')
 publish_button = mari.actions.create('Publish', 'publish.go()')
 
 mari.menus.addAction(export_all_button, "MainWindow/&BYU Tools/Tex Export")
