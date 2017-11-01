9d1f3bad3d90611ee786e17f4b23311f8c9ae29f
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2017-11-01 15:43:56 -0600

    perpare tractor for renderman

--- houdini-tools/tractor_shelf.py
+++ houdini-tools/tractor_shelf.py
@@ -1,7 +1,5 @@
 from PySide2 import QtGui, QtWidgets, QtCore
-import pyqt_houdini
 from TrHttpRPC import TrHttpRPC
-import getpass
 import urllib
 import datetime
 import os
@@ -9,82 +7,95 @@ import re
 import shutil
 import hou
 from byugui import message_gui
-
-# Project name (e.g. owned, ramshorn)
-project_name = "dusk"
-# Nodes to export
-mantra_nodes = []
+from byugui.assemble_gui import AssembleWindow
+from byuam import Project, Environment
 
 # Creates the dialog box
-class ExportDialog(QDialog):
-	empty_text = "delete me"
-	def __init__(self, parent=None):
-		QDialog.__init__(self, parent)
-		vbox = QVBoxLayout()
-		# Job name
-		self.job_name = QLineEdit(self.empty_text)
-		self.job_name.selectAll()
-		name_layout = QHBoxLayout()
-		name_layout.addWidget(QLabel("Job Name:"))
-		name_layout.addWidget(self.job_name)
-		vbox.addLayout(name_layout)
+class ExportDialog(QtWidgets.QWidget):
+
+	finished = QtCore.Signal()
+
+	def __init__(self, parent, renderNodes):
+		super(ExportDialog, self).__init__()
+		self.parent = parent
+		self.project = Project()
+		self.environment = Environment()
+		self.renderNodes = renderNodes
+		self.initUI()
+
+	def initUI(self):
+		# Window layout
+		self.setFixedSize(360, 420)
+		self.setWindowTitle("Confirm Export")
+		main_layout = QtWidgets.QVBoxLayout()
+		self.setLayout(main_layout)
+
+		# Job Name Input Widget
+		self.jobName = QtWidgets.QLineEdit("DeleteMe")
+		self.jobName.selectAll()
+		name_layout = QtWidgets.QHBoxLayout()
+		name_layout.addWidget(QtWidgets.QLabel("Job Name:"))
+		name_layout.addWidget(self.jobName)
+		main_layout.addLayout(name_layout)
+
+		# Priority and Start Time
 		# Priority
-		self.priority = QComboBox()
+		self.priority = QtWidgets.QComboBox()
 		priority_opt = ["Very Low", "Low", "Medium", "High", "Very High"]
 		for opt in priority_opt:
 			self.priority.addItem(opt)
 		self.priority.setCurrentIndex(2)
 		# Begin time
-		self.delay = QComboBox()
+		self.delay = QtWidgets.QComboBox()
 		delay_opts = ["Immediate", "Manual", "Delayed"]
 		for opt in delay_opts:
 			self.delay.addItem(opt)
 		self.delay.currentIndexChanged.connect(self.delaytime)
 		# Combo box options layout
-		opts_layout = QHBoxLayout()
-		opts_layout.addWidget(QLabel("Priority:"))
+		opts_layout = QtWidgets.QHBoxLayout()
+		opts_layout.addWidget(QtWidgets.QLabel("Priority:"))
 		opts_layout.addWidget(self.priority)
-		opts_layout.addWidget(QLabel("Begin:"))
+		opts_layout.addWidget(QtWidgets.QLabel("Begin:"))
 		opts_layout.addWidget(self.delay)
 		opts_layout.addStretch()
-		vbox.addLayout(opts_layout)
+		main_layout.addLayout(opts_layout)
+
 		# Time-frame selection for delayed jobs
-		self.delay_time = QLineEdit("5")
-		self.delay_unit = QComboBox()
+		self.delay_time = QtWidgets.QLineEdit("5")
+		self.delay_unit = QtWidgets.QComboBox()
 		delay_unit_opts = ["mins","hours","days"]
 		for opt in delay_unit_opts:
 			self.delay_unit.addItem(opt)
 		self.delay_unit.setCurrentIndex(0)
 		# Delay time layout
-		self.delay_layout = QHBoxLayout()
-		self.delay_layout.addWidget(QLabel("Delay time:"))
+		self.delay_layout = QtWidgets.QHBoxLayout()
+		self.delay_layout.addWidget(QtWidgets.QLabel("Delay time:"))
 		self.delay_layout.addWidget(self.delay_time)
 		self.delay_layout.addWidget(self.delay_unit)
 		self.delay_layout.addStretch()
 		self.delaytime(0)
-		vbox.addLayout(self.delay_layout)
+		main_layout.addLayout(self.delay_layout)
+
 		# Mantra node selection list
-		self.select = QListWidget()
-		self.select.setSelectionMode(QAbstractItemView.ExtendedSelection)
-		for node in mantra_nodes:
-			item = QListWidgetItem(node.name())
+		self.select = QtWidgets.QListWidget()
+		self.select.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+		for node in self.renderNodes:
+			item = QtWidgets.QListWidgetItem(node.name())
 			self.select.addItem(item)
 			# must come after adding to widget
 			item.setSelected(True)
-		vbox.addWidget(self.select)
+		main_layout.addWidget(self.select)
+
 		# Buttons
-		self.export_btn = QPushButton('Export')
+		self.export_btn = QtWidgets.QPushButton('Export')
 		self.export_btn.clicked.connect(self.export)
-		self.cancel_btn = QPushButton('Cancel')
+		self.cancel_btn = QtWidgets.QPushButton('Cancel')
 		self.cancel_btn.clicked.connect(self.close)
-		btn_layout = QHBoxLayout()
+		btn_layout = QtWidgets.QHBoxLayout()
 		btn_layout.addWidget(self.cancel_btn)
 		btn_layout.addWidget(self.export_btn)
-		vbox.addLayout(btn_layout)
-		# Window layout
-		self.setFixedSize(360, 420)
-		self.setWindowTitle("Confirm Export")
-		self.setLayout(vbox)
+		main_layout.addLayout(btn_layout)
+
 
 	# Show/Hide the delay-time options
 	def delaytime(self, index):
@@ -99,24 +110,28 @@ class ExportDialog(QDialog):
 	# Export selected mantra nodes for rendering
 	def export(self):
 		self.close()
-		# Create a folder specifically for this job
-		user = getpass.getuser()
+
+		# Get user, project, and time info so we can make a temp folder
+		user = self.environment.get_current_username()
+		projectName = self.project.get_name()
 		time_now = datetime.datetime.now()
-	#Changed the ifd_dir to match Taijitu filepaths - Nick Evans 10/12/2016
-		ifd_dir = "/groups/"+project_name+"/ifds/"+user+"_"+time_now.strftime("%m%d%y_%H%M%S")
-	print "ifd_dir", ifd_dir, " mantra_nodes size: ", len(mantra_nodes)
-		os.makedirs(ifd_dir)
+
+		#Make a temp folder for the rib files based on the user and the current time
+		ribDir = "/groups/"+projectName+"/ribs/"+user+"_"+time_now.strftime("%m%d%y_%H%M%S")
+		print "ribDir", ribDir, " renderNodes size: ", len(self.renderNodes)
+		os.makedirs(ribDir)
+
 		# Sanitize job title
-		title = re.sub(r'[{}"\']', "", str(self.job_name.text())).strip(' \t\n\r')
+		title = re.sub(r'[{}"\']', "", str(self.jobName.text())).strip(' \t\n\r')
 		if len(title) == 0:
 			title = self.empty_text
-		# This is the alfred/tractor script we send to engine
-		job_script = "Job -title {"+title+"} -metadata {Spooled by "+user+"} -init {\n\tAssign ifddir {"+ifd_dir+"}\n} -service {PixarRender} -subtasks {"
-		# Loop through each frame of our nodes and create frame tasks
-		index = -1
-		for node in mantra_nodes:
+
+		# This is the start of the alfred/tractor script we send to engine
+		job_script = "Job -title {"+title+"} -metadata {Spooled by "+user+"} -init {\n\tAssign ribdir {"+ribDir+"}\n} -service {PixarRender} -subtasks {"
+
+		# Loop through each frame of our nodes and create frame tasks and append it to the job script
+		for index, node in enumerate(self.renderNodes):
 			# Make sure this node was selected for export
-			index = index+1
 			print node.name();
 			if self.select.item(index).isSelected():
 				name = node.name()
@@ -124,19 +139,27 @@ class ExportDialog(QDialog):
 				end = int(node.parm('f2').eval())
 				step = int(node.parm('f3').eval())
 				job_script += "\n\tTask {"+("%s [%d-%d]" % (name, start, end))+"} -subtasks {"
-				# Activate ifd output
-				node.parm('soho_outputmode').set(True)
-				node.parm('soho_diskfile').set(ifd_dir+('/%s_$F04.ifd' % name))
+
+				oldOutputMode = node.parm('rib_outputmode').eval()
+				oldDiskFile = node.parm('soho_diskfile').eval()
+				# Activate rib output
+				node.parm('rib_outputmode').set(True)
+				node.parm('soho_diskfile').set(ribDir+('/%s_$F04.rib' % name))
+
 				# Loop through every frame in framerange
 				for frame in range(start, end+1, step):
-					job_script += "\n\t\tTask {Frame %04d} -cmds { RemoteCmd {./mantra -V a4 -f $ifddir/%s_%04d.ifd} }" % (frame, name, frame)
+					job_script += "\n\t\tTask {Frame %04d} -cmds { RemoteCmd {/opt/pixar/RenderManProServer-21.3/bin/prman -progress $ribdir/%s_%04d.rib} }" % (frame, name, frame)
 					# Render this frame to the ifd file
 					node.render([frame, frame])
 				job_script += "\n\t}"
-				# Deactivate ifd output
-				node.parm('soho_outputmode').set(False)
-				node.parm('soho_diskfile').set("")
-		job_script += "\n} -cleanup { RemoteCmd {rm -rf "+ifd_dir+"/} }"
+
+				# Restore rib output
+				node.parm('soho_outputmode').set(oldOutputMode)
+				node.parm('soho_diskfile').set(oldDiskFile)
+
+		job_script += "\n} -cleanup { RemoteCmd {rm -rf "+ribDir+"/} }"
+
+		# Attempt to spool job, with the option to keep trying
 		choice = True
 		while choice:
 			rc, msg = self.spool(job_script, time_now)
@@ -152,18 +175,18 @@ class ExportDialog(QDialog):
 				break
 		#Cleanup ifd files, if they didn't want to retry
 		if not choice:
-			shutil.rmtree(ifd_dir)
-	#After the export is done clear the mantra_nodes array - Ben DeMann 10/17/2016
-	del mantra_nodes[:]
-	print "Export Finished!"
+			shutil.rmtree(ribDir)
+		message_gui.info("Job sent to Tractor!")
 
 	# Spool the generated script to the engine
 	def spool(self, job_script, time_now):
+		print "Spooling"
 		delay_type = self.delay.currentIndex()
 		# Spool parameters
 		spool_url = "spool?"
 		# Current user will own this job
-		spool_url += "jobOwner=" + urllib.quote("render") #getpass.getuser()
+		spool_url += "jobOwner=" + urllib.quote("render")
+		#TODO Why do we do the render user instead of the current user?? To get the current user use current_user = self.environment.get_current_username()
 		# Location of mantra executable
 		spool_url += "&cwd=" + urllib.quote("/opt/hfs.current/bin/")
 		# Job priority (1=vlo, 2=lo, 3=med, 4=hi, 5=vhi)
@@ -194,49 +217,10 @@ class ExportDialog(QDialog):
 			xheaders={"Content-Type": "application/tractor-spool"}
 		)
 
-def go():
-	# Get all mantra nodes for export
-	for node in hou.node("/out").children():
-		# Make sure this is a mantra node, and not something else
-		#if node.type().name() == "ifd":
-		#EDIT FOR TAIJITU, remove generic mantra reference and add reference to cameras digital asset - Trevor Barrus 10/11/16
-		if node.type().name() == "dusk_cameras":
-			# Check each checkbox 1 for true (render) 0 for false (do not render) - Nick Evans 10/11/2016
-			ten_checkbox = node.parm('tenCheck').evalAsInt()
-			jampa_checkbox = node.parm('jampaCheck').evalAsInt()
-			background_checkbox = node.parm('backgroundCheck').evalAsInt()
-			foreground_checkbox = node.parm('foregroundCheck').evalAsInt()
-			entrance_checkbox = node.parm('entranceCheck').evalAsInt()
-			props_checkbox = node.parm('propsCheck').evalAsInt()
-			effects_checkbox = node.parm('effectsCheck').evalAsInt()
-			shadow_checkbox = node.parm('parm').evalAsInt()
-
-			#Check the checkboxes, if checked for render add to mantra_nodes - Nick Evans 10/11/16
-			if ten_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_ten"))
-			if jampa_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_jampa"))
-			if background_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_background"))
-			if foreground_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_foreground"))
-			if entrance_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_entrance"))
-			if props_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_props"))
-			if effects_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_effects"))
-		if shadow_checkbox > 0:
-				mantra_nodes.append(node.node("dusk_shadow"))
-			print "Size of mantra_nodes: ", len(mantra_nodes)
-
-
-	# No valid nodes, display error message
-	if (len(mantra_nodes) == 0):
-		hou.ui.displayMessage(("Yo, there ain't nothin for me to render!\n\n"
-							"Make sure you have selected the layers you would like to render."),   severity=hou.severityType.Warning)
-	# Show confirmation dialog
-	else:
-		dialog = ExportDialog()
-		dialog.show()
-		pyqt_houdini.exec_(dialog)
+def go(renderNodes):
+	# Then show the dialog
+	global dialog
+	renderNodes = []
+	renderNodes.append(hou.node("/out/ris1"))
+	dialog = ExportDialog(hou.ui.mainQtWindow(), renderNodes)
+	dialog.show()
