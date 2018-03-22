ea4618aa3eed1de897f30fb1987180d1893a989a
Benjamin Nelson DeMann <benjamin.demann@gmail.com>
2018-03-22 11:57:08 -0600

    you can add mulitple agent references at a time

--- maya-tools/shelf/scripts/reference.py
+++ maya-tools/shelf/scripts/reference.py
@@ -1,10 +1,12 @@
 from byugui.reference_gui import ReferenceWindow
+from byugui import message_gui
 from byuam.environment import Department
 from byuam import byuutil
 import pymel.core as pm
 from PySide2 import QtWidgets
 import maya.OpenMayaUI as omu
 import os
+import time
 
 maya_reference_dialog = None
 
@@ -65,69 +67,78 @@ def referenceCrowdCycle(paths):
 			print 'The cycle doesn\'t exist. That seems weird.', cycle
 			print os.path.exists(str(cycle)), 'this is another shot'
 			return
-		else:
-			print 'Well I guiess it is working'
 		fileName = os.path.basename(cycle)
 		#The file is going to be an alembic so we can drop the last four characters '.abc' to get the file name
 		cycleName = fileName[:len(fileName)-4]
 
-		millis = byuutil.timestampThisYear()
-		namespace = cycleName + str(millis)
+		invalidInput = True
+		while(invalidInput):
+			try:
+				refCount = int(message_gui.input('How many times do you want to reference this cycle?'))
+			except ValueError:
+				message_gui.error('Please enter a number')
+				continue
+			invalidInput = False
 
-		cycleRefGroup = namespace + 'RNgroup'
-		cycleControls = namespace + '_controls'
-		offset = 'offset'
-		speed = 'speed'
-		cycleType = 'cycleType'
-		translate = 'translate'
-		rotate = 'rotate'
-		scale = 'scale'
-		refAlembicNode = namespace + ':' + cycleName + '_AlembicNode'
-		refAlembicOffset = refAlembicNode + '.' + offset
-		refAlembicSpeed = refAlembicNode + '.' + speed
-		refAlembicCycleType = refAlembicNode + '.' + cycleType
-		controlAlembicOffset = cycleControls + '.' + offset
-		controlAlembicSpeed = cycleControls + '.' + speed
+		for i in range(refCount):
+			time.sleep(1) # sleep for a bit to make sure out namespace is unique
+			millis = byuutil.timestampThisYear()
+			namespace = cycleName + str(millis)
 
-		cycleRefTranslate = cycleRefGroup + '.' + translate
-		cycleRefScale = cycleRefGroup + '.' + scale
-		cycleRefRotate = cycleRefGroup + '.' + rotate
+			cycleRefGroup = namespace + 'RNgroup'
+			cycleControls = namespace + '_controls'
+			offset = 'offset'
+			speed = 'speed'
+			cycleType = 'cycleType'
+			translate = 'translate'
+			rotate = 'rotate'
+			scale = 'scale'
+			refAlembicNode = namespace + ':' + cycleName + '_AlembicNode'
+			refAlembicOffset = refAlembicNode + '.' + offset
+			refAlembicSpeed = refAlembicNode + '.' + speed
+			refAlembicCycleType = refAlembicNode + '.' + cycleType
+			controlAlembicOffset = cycleControls + '.' + offset
+			controlAlembicSpeed = cycleControls + '.' + speed
 
-		controlAlembicTranslate = cycleControls + '.' + translate
-		controlAlembicScale = cycleControls + '.' + scale
-		controlAlembicRotate = cycleControls + '.' + rotate
+			cycleRefTranslate = cycleRefGroup + '.' + translate
+			cycleRefScale = cycleRefGroup + '.' + scale
+			cycleRefRotate = cycleRefGroup + '.' + rotate
 
-		pm.system.createReference(cycle, groupReference=True, namespace=namespace)
+			controlAlembicTranslate = cycleControls + '.' + translate
+			controlAlembicScale = cycleControls + '.' + scale
+			controlAlembicRotate = cycleControls + '.' + rotate
 
-		node = pm.ls(cycleRefGroup)[0]
-		circ = pm.circle(r=0.25,nr=(0, 1, 0), n=cycleControls)[0]
-		parentGroupName = namespace + 'agent'
-		group = pm.group(name=parentGroupName, em=True)
+			pm.system.createReference(cycle, groupReference=True, namespace=namespace)
 
-		pm.parent(circ, group)
-		pm.parent(node, group)
+			node = pm.ls(cycleRefGroup)[0]
+			circ = pm.circle(r=0.25,nr=(0, 1, 0), n=cycleControls)[0]
+			parentGroupName = namespace + 'agent'
+			group = pm.group(name=parentGroupName, em=True)
 
-		if circ.hasAttr(offset):
-			circ.deleteAttr(offset)
-		if circ.hasAttr(speed):
-			circ.deleteAttr(speed)
-		circ.addAttr(offset, at='double', hidden=False, dv=0.0, k=True)
-		circ.addAttr(speed, at='double', hidden=False, dv=1.0, k=True)
+			pm.parent(circ, group)
+			pm.parent(node, group)
 
-		# Add agent tag so the exporter can easily find it
-		crowdAgentFlag = 'BYU_Crowd_Agent_Flag'
-		if group.hasAttr(crowdAgentFlag):
-			group.deleteAttr(crowdAgentFlag)
-		group.addAttr(crowdAgentFlag, at=bool, hidden=False, dv=True, k=True)
+			if circ.hasAttr(offset):
+				circ.deleteAttr(offset)
+			if circ.hasAttr(speed):
+				circ.deleteAttr(speed)
+			circ.addAttr(offset, at='double', hidden=False, dv=0.0, k=True)
+			circ.addAttr(speed, at='double', hidden=False, dv=1.0, k=True)
+
+			# Add agent tag so the exporter can easily find it
+			crowdAgentFlag = 'BYU_Crowd_Agent_Flag'
+			if group.hasAttr(crowdAgentFlag):
+				group.deleteAttr(crowdAgentFlag)
+			group.addAttr(crowdAgentFlag, at=bool, hidden=False, dv=True, k=True)
 
 
-		# When passing in arguments to connectAttr remember that attr 1 controls attr 2
-		pm.connectAttr(controlAlembicOffset, refAlembicOffset)
-		pm.connectAttr(controlAlembicSpeed, refAlembicSpeed)
-		pm.setAttr(refAlembicCycleType, 1)
-		pm.connectAttr(controlAlembicTranslate, cycleRefTranslate)
-		pm.connectAttr(controlAlembicRotate, cycleRefRotate)
-		pm.connectAttr(controlAlembicScale, cycleRefScale)
+			# When passing in arguments to connectAttr remember that attr 1 controls attr 2
+			pm.connectAttr(controlAlembicOffset, refAlembicOffset)
+			pm.connectAttr(controlAlembicSpeed, refAlembicSpeed)
+			pm.setAttr(refAlembicCycleType, 1)
+			pm.connectAttr(controlAlembicTranslate, cycleRefTranslate)
+			pm.connectAttr(controlAlembicRotate, cycleRefRotate)
+			pm.connectAttr(controlAlembicScale, cycleRefScale)
 
 
 def go(useNamespace=False):
