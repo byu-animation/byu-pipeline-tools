ff2626080f5e1f32442a667b32b456360a57c4a0
Alexander Charles Neville <masq1@aladdin.cs.byu.edu>
2018-05-17 15:37:24 -0600

    modify slack to maya tool for quotes

--- byugui/inspire_quote_gui.py
+++ byugui/inspire_quote_gui.py
@@ -5,6 +5,8 @@ import os
 import traceback
 import json
 import random
+import requests # added by Alex as a test
+from requests.api import get
 try:
 	from PySide import QtGui as QtWidgets
 	from PySide import QtGui as QtGui
@@ -24,7 +26,9 @@ class QuoteWindow(QtWidgets.QWidget):
 		self.parent = parent
 		quotes_path = os.path.join(os.environ['BYU_TOOLS_DIR'], 'byugui', 'assets', 'inspire-quotes.json')
 		quote_json = file(quotes_path)
+		#self.addQuotesFromSlack(quote_json) # under construction
 		self.quoteData = json.loads(quote_json.read())['quotes']
+		self.addQuotesFromSlack() # still under construction
 		self.initUI()
 
 	def initUI(self):
@@ -60,20 +64,67 @@ class QuoteWindow(QtWidgets.QWidget):
 		index = random.randrange(0,len(self.quoteData))
 		return self.quoteData[index]
 
-	def populateQuote(self):
-		quoteInfo = self.getQuote()
-
-		self.quote.setText(quoteInfo["quote"] + "\n\t-" + quoteInfo["author"])# + "\n\n\n Submitted by: " + quoteInfo["contributor"])
-		image_path = os.path.join(os.environ['BYU_TOOLS_DIR'], 'byugui', 'assets', 'images', quoteInfo['image'])
-		pixmap = QtGui.QPixmap(image_path)
+# Scales image to proper size. Minimizes code duplication.
+	def scaleImage(self, pixmap):
 		scaled = pixmap.scaledToWidth(self.size().width()/2)
 		print scaled
 		print "\n" + str(self.size()) + " " + str(self.size().width())
 		self.img.setPixmap(scaled)
 
+	def populateQuote(self):
+		quoteInfo = self.getQuote()
+		self.quote.setText(quoteInfo["quote"] + "\n\t-" + quoteInfo["author"])# + "\n\n\n Submitted by: " + quoteInfo["contributor"])
+		# If we obtained the quote from Slack, this grabs the avatar of the user who posted the quote.
+		if "slack" in quoteInfo['image']:
+			path = quoteInfo['image']
+			res = get(path)
+			pixmap = QtGui.QPixmap()
+			pixmap.loadFromData(res.content)
+			self.scaleImage(pixmap)
+		else:
+			image_path = os.path.join(os.environ['BYU_TOOLS_DIR'], 'byugui', 'assets', 'images', quoteInfo['image'])
+			pixmap = QtGui.QPixmap(image_path)
+			self.scaleImage(pixmap)
+
 	def closeEvent(self, event):
 		self.finished.emit()
 		event.accept()
+
+# Gets image to use for each Slack quote.
+	def getImageForSlackQuote(self, userID):
+		# We will send another GET request to the slack API.
+		URL = "https://slack.com/api/users.profile.get?token=xoxp-262198519712-364750013863-363181322081-b04dd4517e6975431c598decc65c85fd&user=" + userID
+		result = get(URL)
+		jsonData = result.json()
+		imageID = jsonData['profile']['image_1024']
+		return imageID
+
+# Pulls quotes from the all_the_quotes Slack channel.
+	def addQuotesFromSlack(self):
+		# Authentication via Dandbot's user account. Dandbot isn't actually a bot, but rather a user registered to the Slack workspace.
+		URL = "https://slack.com/api/channels.history?token=xoxp-262198519712-364750013863-363181322081-b04dd4517e6975431c598decc65c85fd&channel=C98MS1YH1"
+		# Send a GET request to the Slack service, and convert it into a JSON object.
+		result = get(URL)
+		myJson = result.json()
+		# Iterate through all the received messages from Slack.
+		for message in myJson['messages']:
+			# Ensures that we only pull quotes from the chat, while omitting "user_x joined #all_the_quotes" messages.
+			if "-" in message['text']:
+				userID = message['user']
+				print "userID: ", userID
+				img = self.getImageForSlackQuote(userID)
+				# rsplit accounts for quotes that might have hyphens in them, such as "flaming hell-octo-copter".
+				data = message['text'].rsplit("-", 1)
+				author = data[1]
+				quote = data[0]
+				# Format JSON object for appending to the JSON array.
+				toAppendToJson = ("{\"author\": \"" + author + "\","
+					+ "\"quote\": " + quote + ","
+					+ "\"image\": \"" + img + "\","
+					+ "\"contributor\": \"Alex Neville and Dandbot\"}")
+				# Appends data to the JSON object at runtime, so we don't have to create a database of already uploaded quotes.
+				jsonDict = json.loads(toAppendToJson)
+				self.quoteData.append(jsonDict)
 
 if __name__ == '__main__':
 	app = QtWidgets.QApplication(sys.argv)
